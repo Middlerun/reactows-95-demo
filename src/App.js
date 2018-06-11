ec551ec24ebe8c60680e5e69858c54b8fad4a0e4
Eddie McLean <middlerun@gmail.com>
2018-06-11 21:21:49 +1000

    Update lib and demo code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 
 import {
-  Shell,
   Desktop,
+  Folder,
   IconArea,
   IconRegular,
+  Shell,
   Taskbar,
   TaskbarItem,
   WindowLayer,
-  Folder,
+  WordPad,
+  ICON_FOLDER,
+  ICON_RICH_TEXT,
 } from 'reactows-95'
+
+function action(message) {
+  return () => {
+    console.log(message)
+  }
+}
 
 class App extends Component {
   render() {
@@ -17,20 +26,25 @@ class App extends Component {
       <Shell>
         <Desktop>
           <IconArea desktop iconTextColor="white">
-            <IconRegular label="An icon"/>
-            <IconRegular label="Another icon"/>
-            <IconRegular label="A third icon"/>
+            <IconRegular label="An icon" icon={ICON_FOLDER} onDoubleClick={action('icon double clicked')}/>
+            <IconRegular label="Another icon" onDoubleClick={action('icon double clicked')}/>
+            <IconRegular label="A third icon" onDoubleClick={action('icon double clicked')}/>
           </IconArea>
           <WindowLayer>
             <Folder title="A window" hasFocus>
-              {(new Array(30)).fill(1).map((val, i) => <IconRegular label="And YOU get an icon!" key={i}/>)}
+              {(new Array(30)).fill(1).map((val, i) => <IconRegular label="And YOU get an icon!" icon={ICON_RICH_TEXT} key={i}/>)}
             </Folder>
+            <WordPad hasFocus>
+              <h1>Content!</h1>
+              <p>Here's some content. Here's some content. Here's some content. Here's some content. Here's some content. Here's some content.</p>
+              <img src="http://lorempixel.com/400/300/cats/" alt="Obligatory cat photo"/>
+            </WordPad>
           </WindowLayer>
         </Desktop>
-        <Taskbar>
-          <TaskbarItem title="A taskbar item"/>
-          <TaskbarItem title="Another taskbar item"/>
-          <TaskbarItem title="Long title on another taskbar item"/>
+        <Taskbar onStartButtonClick={action('start button clicked')}>
+          <TaskbarItem title="A taskbar item" icon={ICON_FOLDER}/>
+          <TaskbarItem title="Another taskbar item" icon={ICON_FOLDER}/>
+          <TaskbarItem title="Long title on another taskbar item" icon={ICON_FOLDER}/>
         </Taskbar>
       </Shell>
     );
